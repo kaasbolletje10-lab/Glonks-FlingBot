8289ea1b9ceaf604de6080b333141abe9db951ae
kaasbolletje10-lab <kaasbolletje10@gmail.com>
2026-05-02 22:07:33 +0200

    Update botfling.lua

--- botfling.lua
+++ botfling.lua
@@ -806,7 +806,7 @@ local function handleCommand(player, msg)
 		createUI("Stand: Spin Stopped")
 
 	elseif cmd == ".cmd" then
-		sendChatMessage(".summon .stop .orbit [n] .tp .tpwall1 .tpwall2 .tpwall3 .wl [user] .unwl [user] .opp [user] .fling [user/all] .stopfling .spin [n] .1 .2 .3 .4 .status .rj .re .script .ver")
+		sendChatMessage(".summon .stop .orbit [n] .tp .tpwall1 .tpwall2  .wl [user] .unwl [user] .opp [user] .fling [user/all] .stopfling .spin [n] .1 .2 .3 .4 .status .rj .re .script .ver")
 		createUI("Command list sent to chat")
 		
 	elseif cmd == ".ver" then
