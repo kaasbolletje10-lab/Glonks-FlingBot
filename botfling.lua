b4d2ecedd6a0afe9f1f0e52ff6c1c65f17982352
kaasbolletje10-lab <kaasbolletje10@gmail.com>
2026-05-03 15:13:21 +0200

    Update botfling.lua

--- botfling.lua
+++ botfling.lua
@@ -796,7 +796,7 @@ local function handleCommand(player, msg)
 		createUI("Script message sent!")
 
 	elseif cmd == ".ver" then
-		sendChatMessage("Version 1.6.0")
+		sendChatMessage("Version 1.6.1")
 		createUI("Version sent to chat")
 
 	elseif cmd == ".cmd" then
