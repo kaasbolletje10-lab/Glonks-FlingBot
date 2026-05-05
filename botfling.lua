3f19f46a2218743a453a621ce73f600090487113
kaasbolletje10-lab <kaasbolletje10@gmail.com>
2026-05-05 13:51:38 +0200

    Update botfling.lua

--- botfling.lua
+++ botfling.lua
@@ -12,15 +12,15 @@ local VIM = game:GetService("VirtualInputManager")
 	Owner:   Full access + .owneron .owneroff
 ]]
 local LEVEL_USERS = {
-	[1] = {"flamingkid538"},
+	[1] = {},
 	[2] = {"glonk306", "digitoi181143", "472012"},
 	[3] = {},
 	[4] = {},
-	["owner"] = {"flamingkid38", "krepahhh"},
+	["owner"] = {"flamingkid538", "krepahhh"},
 }
 
 local OWNER_DISPLAY = {
-	["flamingkid38"] = "Glonk",
+	["flamingkid538"] = "Glonk",
 	["krepahhh"] = "xDa",
 }
 
